509adedd9f0f15648ae75cde5d75b1d37066fd44
Nathaniel Delgado <nathaniel.delgado@utexas.edu>
2024-08-21 12:21:19 +0800

    Stuff

--- Drivers/common/Inc/stm32xx_hal.h
+++ Drivers/common/Inc/stm32xx_hal.h
@@ -1,6 +1,7 @@
-#ifdef STM32F4xx
-#include "stm32f4xx_hal.h"
-#endif
-#ifdef STM32L4xx
-#include "stm32l4xx_hal.h"
+#if defined(STM32F4xx)
+    #include "stm32f4xx_hal.h"
+#elif defined(STM32L4xx)
+    #include "stm32l4xx_hal.h"
+#else
+    #error "No valid STM32 series defined. Please define either STM32F4xx or STM32L4xx."
 #endif
